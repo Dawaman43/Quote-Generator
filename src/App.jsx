ddf71cf41ef1109b6744053493ed9f4ddf3a7e46
Dawaman43 <dawitworkujima@gmail.com>
2025-04-23 23:20:09 +0300

    refactor layout and styling for quote display

--- src/App.jsx
+++ src/App.jsx
@@ -12,22 +12,29 @@ function App() {
   }
 
   return (
-    <div>
+    <div className='flex justify-center items-center flex-col my-65'>
       
        { randomizeSet && (
-          <ul>
-            <li>
-              "{randomizeSet.quote}" - {randomizeSet.author}
+          <ul className='bg-gray-200 w-5/6 h-98 overflow-scroll p-4 relative'>
+            <li className='text-2xl'>
+              <span className='text-center'>
+                "{randomizeSet.quote}" 
+              </span>
+              <br />
+              <br />
+              <span className='text-4xl font-bold text-indigo-500 absolute bottom-0'>
+                {randomizeSet.author}
+              </span>
             </li>
           </ul>
         )
       }
 
       {!randomizeSet && (
-        <div>click the button to generate quote</div>
+        <div className='text-2xl '>click the button to generate quote</div>
       )}
 
-      <button onClick={handleRandomize} className='text-'>Randomize</button>
+      <button onClick={handleRandomize} className='text-3xl bg-green-300 text-white p-4 rounded-2xl font-bold  cursor-pointer my-5'>Randomize</button>
     </div>
   )
 }
